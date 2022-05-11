3790802b8b01bbc23e3474ec1f58d1fe561cea44
Darin755 <darinrawson@gmail.com>
2022-05-10 22:41:36 -0600

    fixed pause/unpause button word choice

--- script.js
+++ script.js
@@ -123,22 +123,22 @@ function saveToFile() {
 function startStop() {
   if (emulator.is_running() == true) {
     emulator.stop();
-    document.getElementById("pause_button").innerHTML = "start";
+    document.getElementById("pause_button").innerHTML = "unpause";
     document.getElementById("save_time").innerHTML = "paused at "+getTimestamp();
   } else {
     emulator.run();
-    document.getElementById("pause_button").innerHTML = "stop";
+    document.getElementById("pause_button").innerHTML = "pause";
     document.getElementById("save_time").innerHTML = "resumed at "+getTimestamp();
   }
 }
 
 function saveRestore() {
-	if(document.getElementById("save_restore").innerHTML == "restore") {
+	if(document.getElementById("save_restore").innerHTML == "restore to previous point") {
 		restore();
-		document.getElementById("save_restore").innerHTML = "save";
+		document.getElementById("save_restore").innerHTML = "save current point";
 	} else {
 		save();
-		document.getElementById("save_restore").innerHTML = "restore";
+		document.getElementById("save_restore").innerHTML = "restore to previous point";
 	}
 }
 
