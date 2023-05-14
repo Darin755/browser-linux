6d0513e47fa56c41ccd4dd932c75a90b2f207a56
Darin755 <darinrawson@gmail.com>
2023-05-13 19:43:51 -0600

    clarified license

--- src/startup.js
+++ src/startup.js
@@ -1,3 +1,21 @@
+/**
+Browser Linux
+Copyright (C) 2022 Darin755
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
 if(!window.WebAssembly) {//if no web assembly
     alert("Your browser is not supported because it doesn't support WebAssembly");
     document.write("unsupported - no Web Assembly");
